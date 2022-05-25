ea2b72fcce97aa1764ca11041bb99ebc3a10d319
alan asmus <alan94vb@gmail.com>
2022-05-25 02:28:35 -0300

    avances 2da entrega

--- js/main.js
+++ js/main.js
@@ -13,7 +13,17 @@ if (user != "" && password != ""){
 } else {
     alert("No ingresaste usuario y contraseña");
 }
- 
+*/
+
+/*
+//Constantes del form de stock
+const form = document.querySelector("form");
+const inputNombre = document.getElementById("nombre").value; 
+const inputPrecioCosto = document.getElementById("precioCosto").value;
+const inputStock = document.getElementById("stock").value;
+const btnGuardar = document.getElementById("btnGuardar").value;
+*/
+
 //Creo la Clase para los productos
 class Producto {
     constructor(nombre, precio, stock) {
@@ -25,18 +35,20 @@ class Producto {
         this.precio = this.precio * 1.4;
     }
 }
+
 //Creo el array contenedor de stock
 const almacen = [];
 
 //Mi stock de productos iniciales
-const producto1 = new Producto ("Lapiz", 50, 10);
-const producto2 = new Producto ("Lapicera negra", 90, 20);
-const producto3 = new Producto ("Lapicera azul", 90, 15);
-console.log(producto1, producto2, producto3);
+const producto1 = new Producto ("Lapiz Bic HB", 50, 10);
+const producto2 = new Producto ("Lapicera Bic negra", 90, 20);
+const producto3 = new Producto ("Lapicera Bic azul", 90, 15);
 
 //Funcion para cargar nuevos productos, para luego sumarlo al array "almacen" 
 function crearProducto () {
-    let nombre = prompt("Ingrese nombre del nuevo producto"), precio = prompt("Ingrese el precio"), stock = prompt("Ingrese Stock");
+    let nombre = document.getElementById("nombre").value; 
+    let precio = document.getElementById("precioCosto").value;
+    let stock = document.getElementById("stock").value;
     let nuevoProducto = new Producto(nombre, precio, stock);
     return nuevoProducto;
 }
@@ -58,9 +70,10 @@ console.log(almacen);
 //Recargo la ganancia al precio de costo inicial
 for (const Producto of almacen) {
     Producto.sumaGanancia();
-    console.log("El precio final es " + Producto.precio);
+    console.log("El precio final de " + Producto.nombre + " es " + Producto.precio);
 }
 
+/*
 //Agrego un metodo de busca "filter", con el objetivo de poder encontrar elementos que cumplan ciertas condiciones en su nombre escribiendolo exactamente como es
 function buscarProducto () {
     let busqueda = prompt("Busca un producto por su nombre");
