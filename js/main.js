d84c0126b1e1dfd16197d5f4a1c9d9ccbb2778e2
alan asmus <alan94vb@gmail.com>
2022-05-27 02:00:46 -0300

    avances en css y js

--- js/main.js
+++ js/main.js
@@ -1,76 +1,53 @@
-/*
-function inicioSesion(mensaje) {
-    let sesion = prompt(mensaje);
-    console.log(sesion);
-    return sesion;
-}
-
-let user = inicioSesion("Ingrese su usuario");
-let password = inicioSesion("Ingrese su contraseña");
-
-if (user != "" && password != ""){
-    alert("Bienvenido/a " + user);
-} else {
-    alert("No ingresaste usuario y contraseña");
-}
-*/
-
-/*
-//Constantes del form de stock
-const form = document.querySelector("form");
-const inputNombre = document.getElementById("nombre").value; 
-const inputPrecioCosto = document.getElementById("precioCosto").value;
-const inputStock = document.getElementById("stock").value;
-const btnGuardar = document.getElementById("btnGuardar").value;
-*/
+//Creo el array contenedor de stock
+const almacen = []; 
 
 //Creo la Clase para los productos
 class Producto {
-    constructor(nombre, precio, stock) {
+    constructor(nombre, precioCosto, stock, precioFinal) {
         this.nombre = nombre;
-        this.precio= parseFloat (precio);
+        this.precioCosto= parseFloat (precioCosto);
         this.stock = parseFloat (stock);
-    }
-    sumaGanancia() {
-        this.precio = this.precio * 1.4;
-    }
-}
-
-//Creo el array contenedor de stock
-const almacen = [];
+        this.precioFinal = this.precioCosto * 1.5;
+    }    
+    /*sumaGanancia() {
+        this.precio = this.precioCosto * 1.4;
+    }*/
+}    
 
 //Mi stock de productos iniciales
 const producto1 = new Producto ("Lapiz Bic HB", 50, 10);
 const producto2 = new Producto ("Lapicera Bic negra", 90, 20);
 const producto3 = new Producto ("Lapicera Bic azul", 90, 15);
 
+cargarProducto(producto1);
+cargarProducto(producto2);
+cargarProducto(producto3);
+console.log(almacen);
+actualizarTabla()
+
 //Funcion para cargar nuevos productos, para luego sumarlo al array "almacen" 
 function crearProducto () {
-    let nombre = document.getElementById("nombre").value; 
-    let precio = document.getElementById("precioCosto").value;
-    let stock = document.getElementById("stock").value;
-    let nuevoProducto = new Producto(nombre, precio, stock);
-    return nuevoProducto;
+    nombre = document.querySelector("#nombre").value; 
+    precioCosto = document.querySelector("#precioCosto").value;
+    stock = document.querySelector("#stock").value;
+    let nuevoProducto = new Producto(nombre, precioCosto, stock);
+    almacen.push(nuevoProducto);
+    actualizarTabla();
+    console.log(almacen);
 }
+
 function cargarProducto(nuevoProducto) {
     almacen.push(nuevoProducto);
 }
 
-//Defino nuevoProducto para luego sumarlo al array
-let nuevoProducto = crearProducto();
-
-console.log(almacen);
-
-cargarProducto(producto1);
-cargarProducto(producto2);
-cargarProducto(producto3);
-cargarProducto(nuevoProducto);
-console.log(almacen);
-
-//Recargo la ganancia al precio de costo inicial
-for (const Producto of almacen) {
-    Producto.sumaGanancia();
-    console.log("El precio final de " + Producto.nombre + " es " + Producto.precio);
+function actualizarTabla(){
+    let html = "";
+    html += "<tr><th>Nombre</th><th>Precio Costo</th><th>Stock</th><th>Precio Final</th></tr>";
+    for (const producto of almacen) {
+        html += "<tr><td>"+producto.nombre+"</td><td>"
+        +producto.precioCosto+"</td><td>"+producto.stock+"</td><td>"+producto.precioFinal+"</td></tr>";
+    }
+    document.querySelector("#tabla").innerHTML = html;
 }
 
 /*
