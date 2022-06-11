d0a7cd33d4f34e98dc9f8af42b65d4221818ede4
alan asmus <alan94vb@gmail.com>
2022-06-11 16:27:14 -0300

    Ajax

--- js/main.js
+++ js/main.js
@@ -1,32 +1,37 @@
-//Creo el array contenedor de stock
-let almacen = []; 
-if(localStorage.getItem("almacen") == null){
-    almacen = [];
-} else {
-    almacen = JSON.parse(localStorage.getItem("almacen"));
-}
-
-//Creo la Clase para los productos
+//Clase para los productos
 class Producto {
     constructor(nombre, precioCosto, stock, precioFinal) {
         this.nombre = nombre;
-        this.precioCosto= parseFloat (precioCosto);
-        this.stock = parseFloat (stock);
+        this.precioCosto = parseFloat(precioCosto);
+        this.stock = parseFloat(stock);
         this.precioFinal = this.precioCosto * 1.5;
-    }    
-}    
+    }
+}
 
-actualizarTabla()
+//Llamo  al array de productos con fetch
 
-//Funcion para cargar nuevos productos, para luego sumarlo al array "almacen" 
-function crearProducto () {
-    nombre = document.querySelector("#nombre").value; 
+const pedirData = async () => {
+    try {
+        const resp = await fetch("data.json");
+        console.log(resp);
+        const data = await resp.json();
+        console.log(data);
+        //Realizo un forof con data.almacen para así si generar el precioFinal
+        for(const prod of data.almacen) {
+            cargarProducto(new Producto(prod.nombre, prod.precioCosto, prod.stock));
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function crearProducto() {
+    nombre = document.querySelector("#nombre").value;
     precioCosto = document.querySelector("#precioCosto").value;
     stock = document.querySelector("#stock").value;
     let nuevoProducto = new Producto(nombre, precioCosto, stock);
     almacen.push(nuevoProducto);
     actualizarTabla();
-    //console.log(almacen);
     localStorage.setItem("almacen", JSON.stringify(almacen));
     Swal.fire({
         title: 'Stock Cargado Exitosamente!',
@@ -44,13 +49,54 @@ function cargarProducto(nuevoProducto) {
     almacen.push(nuevoProducto);
 }
 
-function actualizarTabla(){
+function actualizarTabla() {
     let html = "";
     html += "<tr><th>Nombre</th><th>Precio Costo</th><th>Stock</th><th>Precio Final</th></tr>";
-    for (const producto of almacen) {
-        html += "<tr><td>"+producto.nombre+"</td><td>"
-        +producto.precioCosto+"</td><td>"+producto.stock+"</td><td>"+producto.precioFinal+"</td></tr>";
-    }
-    document.querySelector("#tabla").innerHTML = html;
+    //Try y catch para captar errores
+    try {
+        for (const producto of almacen) {
+            html += "<tr><td>" + producto.nombre + "</td><td>"
+            + producto.precioCosto + "</td><td>" + producto.stock + "</td><td>" + producto.precioFinal + "</td></tr>";
+            console.log(html);
+        }
+        document.querySelector("#tabla").innerHTML = html;
+    } catch(err) {
+        console.log(err);
+    }    
 }
 
+let almacen = [];
+
+//Creo esta funcion para pedir la data y que espera a recibir su respuesta
+const cargarData = async () => {
+    await pedirData()
+    actualizarTabla();
+}
+cargarData();
+
+
+
+
+
+
+
+
+
+/*const pedirData = () => {
+    fetch("https://swapi.dev/api/people").then((response) => {
+        console.log(response);
+        response.json().then((data) => {
+            console.log(data);
+            let html = "";
+            html += "<tr><th>Nombre</th><th>Sexo</th><th>Pelo</th><th>Piel</th></tr>";
+            for (const personaje of data.results) {
+                html += "<tr><td>" + personaje.name + "</td><td>"
+                    + personaje.gender + "</td><td>" + personaje.hair_color + "</td><td>" + personaje.skin_color + "</td></tr>";
+            }
+            document.querySelector("#tabla").innerHTML = html;
+        })
+    });
+
+}
+
+pedirData();*/
